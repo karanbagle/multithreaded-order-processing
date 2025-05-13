f08de8604b62fd53b46f0c606e0b596c0ca7a685
Karan Bagle <karanbagle420@GMAIL.COM>
2025-05-12 20:05:35 -0400

    Step 2: Added CountDownLatch to wait for order completion

--- order-processing/src/main/java/com/karan/Main.java
+++ order-processing/src/main/java/com/karan/Main.java
@@ -1,17 +1,31 @@
 package com.karan;
 import com.karan.order.model.Order;
 import com.karan.order.service.OrderProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(4); // pool with 4 threads
+        final  Logger logger = LoggerFactory.getLogger(Main.class);
+        int totalOrders = 10;
+        CountDownLatch countDownLatch = new CountDownLatch(totalOrders); //A one-time use countdown object used for coordination
+
 
         for(int i=1; i<=10; i++) {
             Order order = new Order(i,"Item-"+i);
-            executorService.submit(new OrderProcessor(order));
+            executorService.submit(new OrderProcessor(order, countDownLatch));
         }
+        try{
+            countDownLatch.await(); //wait for all tasks to finish
+        }catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        logger.info("All orders processed!");
         executorService.shutdown();
     }
 }
--- order-processing/src/main/java/com/karan/order/service/OrderProcessor.java
+++ order-processing/src/main/java/com/karan/order/service/OrderProcessor.java
@@ -3,13 +3,17 @@ import com.karan.order.model.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.CountDownLatch;
+
 
 public class OrderProcessor implements  Runnable {
     private static final Logger logger = LoggerFactory.getLogger(OrderProcessor.class);
     private final Order order;
+    private final CountDownLatch countDownLatch;
 
-    public OrderProcessor(Order order) {
+    public OrderProcessor(Order order, CountDownLatch countDownLatch) {
         this.order = order;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
@@ -20,7 +24,10 @@ public class OrderProcessor implements  Runnable {
         }catch (InterruptedException e){
             Thread.currentThread().interrupt();
             logger.error("Thread interrupted while processing order {}", order);
+        }finally {
+            logger.info("Processing completed {}", order);
+            countDownLatch.countDown(); //Called when a task is complete
+
         }
-        logger.info("Completed {}", order);
     }
 }
